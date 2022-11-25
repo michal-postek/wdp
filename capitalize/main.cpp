7cbc096eb9005b5d44fd9dba90638b57d8a9a17c
michal-postek <michal.postek.3310@gmail.com>
2022-11-25 21:40:20 +0100

    Add files

---
+++ capitalize/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int counter = 0;
+    string text;
+    cout << "Podaj wyraz: ";
+    cin >> text;
+
+    int len = text.length();
+
+    for (int i = 0; i < len; i++) {
+        if (text.substr(i, 1) == "a") {
+            text[i] = 'A';
+            counter++;
+        } else if (text.substr(i, 1) == "b") {
+            text[i] = 'B';
+            counter++;
+        }
+    }
+
+    if (len % 2 != 0) {
+        char middle = text[(len - 1) / 2];
+
+        if (middle != 'a' && middle != 'b') {
+            text[len / 2] = toupper(text[len / 2]);
+            counter++;
+        }
+    }
+
+    cout << "Zmieniony wyraz: " << text << "\n";
+    cout << "Liczba zmian: " << counter << "\n";
+}
